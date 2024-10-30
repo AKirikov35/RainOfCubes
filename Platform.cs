96bcf0ef70dca699e05730029b6de3e45f96fc2a
AKirikov35 <161311981+AKirikov35@users.noreply.github.com>
2024-10-31 00:35:04 +0300

    Platform.cs

--- Platform.cs
+++ Platform.cs
@@ -1,13 +1,6 @@
-using System;
 using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-    public event Action<GameObject> WasCollision;
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag(ObjectTag.Cube.ToString()))
-            WasCollision?.Invoke(collision.gameObject);
-    }
 }
