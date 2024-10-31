6936445fdcbd2db8a46531f5e9905568385da6c8
AKirikov35 <161311981+AKirikov35@users.noreply.github.com>
2024-10-31 11:00:32 +0300

    FallingCube.cs

--- FallingCube.cs
+++ FallingCube.cs
@@ -1,16 +1,28 @@
+using System.Collections;
 using UnityEngine;
 
-[RequireComponent (typeof(Renderer))]
-[RequireComponent (typeof(BoxCollider))]
-[RequireComponent (typeof(Rigidbody))]
+[RequireComponent(typeof(Renderer))]
+[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(Rigidbody))]
 public class FallingCube : MonoBehaviour
 {
     private Renderer _renderer;
+    private Color _defaultColor;
     private bool _hasCollided = false;
+
+    private CubePool _cubePool;
+
+    public void Initialize(CubePool cubePool)
+    {
+        _cubePool = cubePool;
+        _renderer.material.color = _defaultColor;
+        _hasCollided = false;
+    }
 
     private void Awake()
     {
         _renderer = GetComponent<Renderer>();
+        _defaultColor = _renderer.material.color;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -19,8 +31,14 @@ public class FallingCube : MonoBehaviour
         {
             _renderer.material.color = new Color(Random.value, Random.value, Random.value);
             _hasCollided = true;
-            Destroy(gameObject, GetRandomDelay());
+            StartCoroutine(ReturnToPool(GetRandomDelay()));
         }
+    }
+
+    private IEnumerator ReturnToPool(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        _cubePool.ReturnToPool(this);
     }
 
     private float GetRandomDelay()
